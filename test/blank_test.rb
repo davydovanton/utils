5307d33389485d943d88aa401ce528a36792816c
Anton Davydov <antondavydov.o@gmail.com>
2017-01-07 17:12:25 +0300

    Test case

--- test/blank_test.rb
+++ test/blank_test.rb
@@ -15,6 +15,10 @@ describe Hanami::Utils::Blank do
        end
      end
 
+    it { (true if 0...1).must_equal true }
+
+    it { (true if 1..2).must_equal nil }
+
     [Object.new, true, 0, 1, 'a', :book, DateTime.now, Time.now, Date.new, [nil], { nil => 0 }, Set.new([1]),
      Hanami::Utils::Kernel.Symbol(:hello), Hanami::Utils::String.new('foo'),
      Hanami::Utils::Hash.new(foo: :bar)].each do |v|
